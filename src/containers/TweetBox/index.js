fc113a3917493b70c347484f1252fc76548a2f48
Riku Rouvila <riku.rouvila@gmail.com>
2017-02-02 00:21:32 +0200

    fetch monitor content from reddit

--- src/containers/TweetBox/index.js
+++ src/containers/TweetBox/index.js
@@ -5,10 +5,19 @@ import './index.css';
 
 class TweetBox extends Component {
   state = {
-    tweets: []
+    tweets: [],
+    redditThreads: []
   }
   componentDidMount = async () => {
     const tweets = await getTweets();
+
+    fetch('https://www.reddit.com/r/Suomi.json').then((res) => res.json())
+      .then((response) => {
+        this.setState({
+          redditThreads: response.data.children.map(({data: {title, author, id}}) => ({title, author, id}))
+        })
+      })
+
     this.setState({ tweets: tweets });
   }
   render() {
@@ -16,11 +25,11 @@ class TweetBox extends Component {
       <Monitor className="tweet-box">
         <h3>Recent tweets from Spacebook</h3>
         {
-          this.state.tweets.map((tweet) => {
+          this.state.redditThreads.map((tweet) => {
             return (
               <div className="tweet-box__tweet" key={tweet.id}>
-                <strong>{tweet.nick}</strong><br />
-                {tweet.message}<br />
+                <strong>{tweet.author}</strong><br />
+                {tweet.title}<br />
                 ---------------------------
               </div>
             )
