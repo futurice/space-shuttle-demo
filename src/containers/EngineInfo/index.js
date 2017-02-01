2c04c24fc367bab1ae08178097caf26a4e1527f4
Riku Rouvila <riku.rouvila@gmail.com>
2017-02-01 23:43:47 +0200

    add more functionality to engine indo

--- src/containers/EngineInfo/index.js
+++ src/containers/EngineInfo/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Lever from '../../components/Lever';
 import Gauge from '../../components/Gauge';
+import LiquidGauge from '../../components/LiquidGauge';
+import Button from '../../components/Button';
+import { speed$, fuel$, setThrottle, boost } from '../../modules/engine';
 
-import { speed$, fuel$, setThrottle } from '../../modules/engine';
+import './index.css';
 
 class EngineInfo extends Component {
   state = {
@@ -18,9 +21,16 @@ class EngineInfo extends Component {
   }
   render() {
     return (
-      <div>
-        <Gauge label="Speed" value={this.state.speed} />
-        <Lever onChange={this.setThrottle} label="Throttle"></Lever>
+      <div className="engine-info">
+        <div className="engine-info__meters">
+          <Gauge label="Speed" value={this.state.speed} />
+          <LiquidGauge label="Fuel" value={this.state.fuel} />
+        </div>
+        <div className="engine-info__levers">
+          <Lever className="engine-info__throttle" onChange={this.setThrottle} label="Throttle"></Lever>
+          <Lever label="Radio"></Lever>
+          <Button onClick={boost} className="engine-info__boost">Boost</Button>
+        </div>
       </div>
     );
   }
