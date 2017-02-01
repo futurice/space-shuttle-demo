a0b337f14483089df9092ebeb49bc770090d6e53
Riku Rouvila <riku.rouvila@gmail.com>
2017-02-01 11:56:02 +0200

    add first e2e tests and some basic throttle / fuel display functionality

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,17 +3,29 @@ import DashboardRow from './components/DashboardRow';
 import Lever from './components/Lever';
 import LiquidGauge from './components/LiquidGauge';
 
+import { speed$, fuel$, setThrottle } from './modules/engine';
+
 import './Dashboard.css';
 
 class Dashboard extends Component {
+  state = {
+    fuel: 100,
+    speed: 0
+  }
+  componentDidMount = () => {
+    speed$.subscribe((currentSpeed) => this.setState({ speed: currentSpeed }));
+    fuel$.subscribe((currentFuel) => this.setState({ fuel: currentFuel }));
+  }
+  setThrottle = (amount) => {
+    setThrottle(amount);
+  }
   render() {
     return (
       <div className="dashboard">
         <DashboardRow>
-          <LiquidGauge label="Fuel" />
-          <Lever label="Throttle"></Lever>
+          <LiquidGauge label="Fuel" value={this.state.fuel} />
+          <Lever onChange={this.setThrottle} label="Throttle"></Lever>
         </DashboardRow>
-
       </div>
     );
   }
--- src/Dashboard.test.js
+++ src/Dashboard.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Dashboard from './Dashboard';
+import Lever from './components/Lever';
+import LiquidGauge from './components/LiquidGauge';
+
+const wait = (ms) =>
+  new Promise((resolve) => setInterval(resolve, ms));
 
 test('Dashboard contains a throttle lever and a fuel gauge', () => {
   // Render the dashboard to a fake document
   const dashboard = mount(<Dashboard />);
+
+  expect(dashboard.find(Lever)).toHaveLength(1);
+  expect(dashboard.find(LiquidGauge)).toHaveLength(1);
 });
 
+test('The fuel gauge show the current amount of fuel in the fuel tank', () => {
+  const dashboard = mount(<Dashboard />);
+
+  expect(dashboard.find(Lever)).toHaveLength(1);
+  expect(dashboard.find(LiquidGauge).props().value).toBeDefined();
+  expect(dashboard.find(LiquidGauge).props().value).toEqual(100);
+});
+
+test('Going full throttle decreses the amount of fuel', async () => {
+  const dashboard = mount(<Dashboard />);
+
+  dashboard.find(Lever).find('input').simulate('change', { target: { value: 100 }});
+
+  await wait(1000);
+
+  expect(dashboard.find(LiquidGauge).props().value).toBeLessThan(100);
+});
